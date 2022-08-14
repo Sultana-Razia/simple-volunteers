bc913402556b075417ffaa3e9a19f94fa6cc0568
Sultana Razia <sultanarazia483@gmail.com>
2022-08-14 08:17:33 +0600

    useEffect with dependency

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,22 +1,38 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
-import useVolunteers from '../../hooks/useVolunteers';
+// import useVolunteers from '../../hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 import './Main.css'
 
 const Main = () => {
-    const [volunteers, setVolunteers] = useVolunteers();
+    // const [volunteers, setVolunteers] = useVolunteers();
+    const [searchText, setSearchText] = useState('');
     const [searchResult, setSearchResult] = useState([]);
 
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => {
+                const match = data.filter(d => d.title.includes(searchText));
+                setSearchResult(match);
+            })
+    }, [searchText])
+
     const handleSearchChange = event => {
-        const searchText = event.target.value;
-        const match = volunteers.filter(v => v.title.includes(searchText));
-        setSearchResult(match);
+        setSearchText(event.target.value);
+
     }
+
+    // const handleSearchChange = event => {
+    //     const searchText = event.target.value;
+    //     const match = volunteers.filter(v => v.title.includes(searchText));
+    //     setSearchResult(match);
+    // }
 
     return (
         <div>
-            <h2>Volunteer Activities: {volunteers.length}</h2>
+            <h2>Volunteer Activities: {searchResult.length}</h2>
             <div style={{ 'margin': '20px' }}>
                 <input onChange={handleSearchChange} type="text" placeholder='search' />
             </div>
